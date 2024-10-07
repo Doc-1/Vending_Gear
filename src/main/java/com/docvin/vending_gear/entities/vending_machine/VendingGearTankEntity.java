d68f81150efd6c11d43c5158a63e6c30bad4c02e
_Doc <ldoc256@gmail.com>
2024-10-07 11:38:25 -0400

    Added basic movement and attack AI

--- src/main/java/com/docvin/vending_gear/entities/vending_machine/VendingGearTankEntity.java
+++ src/main/java/com/docvin/vending_gear/entities/vending_machine/VendingGearTankEntity.java
@@ -1,13 +1,107 @@
 package com.docvin.vending_gear.entities.vending_machine;
 
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IRangedAttackMob;
+import net.minecraft.entity.ai.EntityAIAttackRanged;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionType;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-public class VendingGearTankEntity extends EntityLiving {
+public class VendingGearTankEntity extends EntityMob implements IRangedAttackMob {
+
+	private int explosionRadius = 3;
 
 	public VendingGearTankEntity(World worldIn) {
 		super(worldIn);
-		// TODO Auto-generated constructor stub
 	}
 
+	public void writeEntityToNBT(NBTTagCompound compound) {
+		super.writeEntityToNBT(compound);
+
+		compound.setByte("ExplosionRadius", (byte) this.explosionRadius);
+	}
+
+	public void readEntityFromNBT(NBTTagCompound compound) {
+		super.readEntityFromNBT(compound);
+		if (compound.hasKey("ExplosionRadius", 99)) {
+			this.explosionRadius = compound.getByte("ExplosionRadius");
+		}
+	}
+
+	@Override
+	public void attackEntityWithRangedAttack(EntityLivingBase target, float distanceFactor) {
+		double d0 = target.posY + (double) target.getEyeHeight() - 1.100000023841858D;
+		double d1 = target.posX + target.motionX - this.posX;
+		double d2 = d0 - this.posY;
+		double d3 = target.posZ + target.motionZ - this.posZ;
+		float f = MathHelper.sqrt(d1 * d1 + d3 * d3);
+		PotionType potiontype = PotionTypes.HARMING;
+
+		if (f >= 8.0F && !target.isPotionActive(MobEffects.SLOWNESS)) {
+			potiontype = PotionTypes.SLOWNESS;
+		} else if (target.getHealth() >= 8.0F && !target.isPotionActive(MobEffects.POISON)) {
+			potiontype = PotionTypes.POISON;
+		} else if (f <= 3.0F && !target.isPotionActive(MobEffects.WEAKNESS) && this.rand.nextFloat() < 0.25F) {
+			potiontype = PotionTypes.WEAKNESS;
+		}
+
+		EntityPotion entitypotion = new EntityPotion(this.world, this,
+				PotionUtils.addPotionToItemStack(new ItemStack(Items.SPLASH_POTION), potiontype));
+		entitypotion.rotationPitch -= -20.0F;
+		entitypotion.shoot(d1, d2 + (double) (f * 0.2F), d3, 0.75F, 8.0F);
+		this.world.playSound((EntityPlayer) null, this.posX, this.posY, this.posZ, SoundEvents.BLOCK_PISTON_EXTEND,
+				this.getSoundCategory(), 1.0F, 0.8F + this.rand.nextFloat() * 0.4F);
+		this.world.spawnEntity(entitypotion);
+	}
+
+	@Override
+	public boolean isInWater() {
+		boolean result = super.isInWater();
+		if (result)
+			explode();
+
+		return result;
+	}
+
+	private void explode() {
+		if (!this.world.isRemote) {
+			boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world, this);
+			float f = 2.0F;
+			this.dead = true;
+			this.world.createExplosion(this, this.posX, this.posY, this.posZ, (float) this.explosionRadius * f, flag);
+			this.world.makeFireworks(this.posX, this.posY, this.posZ, 0, 0, 0, getEntityData());
+			this.setDead();
+		}
+	}
+
+	@Override
+	public void setSwingingArms(boolean swingingArms) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void initEntityAI() {
+		this.tasks.addTask(2, new EntityAIAttackRanged(this, 0.5D, 60, 10.0F));
+		this.tasks.addTask(2, new EntityAIWanderAvoidWater(this, 0.5D));
+		this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+		this.tasks.addTask(3, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false, new Class[0]));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, true));
+	}
 }
